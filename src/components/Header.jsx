8a35b9ec4568ac912eb5efbfc9ebe78d88866f9d
Bilal Mirza <bsohail420@gmail.com>
2023-08-17 20:49:23 +0500

    Created basic header and styles for it. Bootstrap cdn link is added to 'index.html'.

---
+++ src/components/Header.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Header = () => {
+
+    return (
+        <div className="header p-1">
+            <h1 className="text-light m-3">
+                coInfo
+            </h1>
+        </div>
+    )
+}
+export default Header
